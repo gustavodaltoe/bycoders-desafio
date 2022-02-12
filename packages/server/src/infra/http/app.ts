450e9fa2d215ce78e9fe9cd358556f44adccf1cc
gustavodaltoe <gustavodaltoe@hotmail.com>
2022-02-12 02:25:02 -0300

    chore: configure express app

---
+++ packages/server/src/infra/http/app.ts
@@ -0,0 +1,22 @@
+import cors from 'cors';
+import express from 'express';
+
+import { router } from './routes';
+
+const app = express();
+
+app.use(
+  cors({
+    exposedHeaders: ['x-total-count', 'Content-Type', 'Content-Length'],
+  }),
+);
+
+app.use(
+  express.json({
+    type: ['application/json', 'text/plain'],
+  }),
+);
+
+app.use(router);
+
+export { app };
