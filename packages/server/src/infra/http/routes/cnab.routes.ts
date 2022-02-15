34eb32217e427376cf89816dae46ddc75771cff5
gustavodaltoe <gustavodaltoe@hotmail.com>
2022-02-15 20:38:20 -0300

    refactor: remove unused variable

--- packages/server/src/infra/http/routes/cnab.routes.ts
+++ packages/server/src/infra/http/routes/cnab.routes.ts
@@ -1,12 +1,8 @@
 import { adaptRoute } from '@core/infra/adapters/ExpressRouteAdapter';
 import express from 'express';
 import { makeUploadCnabController } from '../factories/controllers/UploadCnabControllerFactory';
-import multer from 'multer';
 import { adaptMiddleware } from '@core/infra/adapters/ExpressMiddlewareAdapter';
 import { makeTextFileUploadMiddleware } from '../factories/middlewares/TextFileUploadMiddlewareFactory';
-
-const storage = multer.memoryStorage();
-const upload = multer({ storage, limits: { fileSize: 100 * 1024 * 1024 } });
 
 const cnabRouter = express.Router();
 
