362fdfc53aec063b01fd734cedbc8315adc6d671
gustavodaltoe <gustavodaltoe@hotmail.com>
2022-02-16 03:49:40 -0300

    chore: run migrations at heroku

--- packages/web/src/services/axios.ts
+++ packages/web/src/services/axios.ts
@@ -1,6 +1,6 @@
 import libAxios from 'axios';
 
-const baseURL = process.env.API_BASE_URL || 'http://localhost:3333';
+const baseURL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3333';
 
 export const axios = libAxios.create({
   baseURL,
